d52209c2501633e55cec4ae4d92cc6aa6f06f772
Christopher <cqian019@gmail.com>
2016-11-13 14:09:42 -0800

    Implemented group joining and leaving

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   get "groups", to: "groups#index"
+  patch "group_join", to: "groups#join", as: "join_group"
+  delete "group_leave", to: "groups#leave", as: "leave_group"
   devise_for :users
   resources :users
 end
