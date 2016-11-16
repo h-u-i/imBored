040765334f0b4066c06f3799e075732d3983c077
Daryus Medora <djmedora@gmail.com>
2016-11-15 17:48:34 -0800

    why is this not working

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20161114005727) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -39,6 +46,13 @@ ActiveRecord::Schema.define(version: 20161114005727) do
     t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_groups", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.index ["group_id"], name: "index_users_groups_on_group_id"
+    t.index ["user_id"], name: "index_users_groups_on_user_id"
   end
 
 end
